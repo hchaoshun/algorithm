25780f587b5ddfe19aff5c86070b2596fe2beb10
Jachin Huang <hcsxiaohan@gmail.com>
2020-02-04 19:56:14 +0800

    todo

--- codinginterviews/028-MoreThanHalfNum_Solution.cpp
+++ codinginterviews/028-MoreThanHalfNum_Solution.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //todo other method?
     int MoreThanHalfNum_Solution(vector<int> numbers) {
         auto iter = find_if(numbers.begin(), numbers.end(), [&numbers](const int num) {
             return 2 * count(numbers.begin(), numbers.end(), num) > numbers.size();
