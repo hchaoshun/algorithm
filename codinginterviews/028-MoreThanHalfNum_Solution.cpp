1127c15e0607b0fd9cc0c26c978492edd0528fbd
Jachin Huang <hcsxiaohan@gmail.com>
2020-02-04 19:54:06 +0800

    028

---
+++ codinginterviews/028-MoreThanHalfNum_Solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int MoreThanHalfNum_Solution(vector<int> numbers) {
+        auto iter = find_if(numbers.begin(), numbers.end(), [&numbers](const int num) {
+            return 2 * count(numbers.begin(), numbers.end(), num) > numbers.size();
+        });
+        return iter == numbers.end() ? 0 : *iter;
+    }
+};
