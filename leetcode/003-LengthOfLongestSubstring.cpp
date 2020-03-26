749ad81c793e588376f4c2b2f42cb35097be8673
Jachin Huang <hcsxiaohan@gmail.com>
2020-03-26 19:25:40 +0800

    003

--- leetcode/003-LengthOfLongestSubstring.cpp
+++ leetcode/003-LengthOfLongestSubstring.cpp
@@ -1,19 +1,21 @@
+//遍历遇到重复值时，记录此时max，然后从前一个重复值+1作为起始
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        const int MAX_ASII = 256;
-        int record[MAX_ASII];
-        fill(record, record + MAX_ASII, -1);
-        int max_len = 0;
+        int find[256];
+        memset(find, -1, sizeof(find));
         int start = 0;
-        for (int i = 0; i < s.size(); ++i) {
-            //record[s[i]] != -1，start会倒退
-            if (record[s[i]] >= start) {
-                max_len = max(i - start, max_len);
-                start = record[s[i]] + 1;
+        int max_len = 0;
+        for (int i = 0; i < s.size(); i++) {
+            //注意如果没有后面的条件start可能会回退
+            //考虑abba，当走到最后一个a是，start会从2退到1，显然错误
+            //start应该总是小于find[s[i]]
+            if (find[s[i]] != -1 && start <= find[s[i]]) {
+                max_len = max(max_len, i - start);
+                start = find[s[i]] + 1;
             }
-            record[s[i]] = i;
+            find[s[i]] = i;
         }
-        return max((int)(s.size() - start), max_len);
+        return max((int)s.size() - start, max_len); //考虑到没有重复的情况
     }
 };
