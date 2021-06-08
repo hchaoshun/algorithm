e3c945dd7d536fdaae9b56d89e41a5d5ea25447d
hchaoshun <hcsxiaohan@gmail.com>
2021-06-08 21:08:17 +0800

    update

--- codinginterviews/021-IsPopOrder.cpp
+++ codinginterviews/021-IsPopOrder.cpp
@@ -15,3 +15,35 @@ public:
         return s.empty();
     }
 };
+
+
+//2021-06-04
+class Solution {
+public:
+    bool IsPopOrder(vector<int> pushV,vector<int> popV) {
+        int push = 0, pop = 0;
+        stack<int> s;
+        while (pop < popV.size()) {
+            if (push < pushV.size() && pushV[push] != popV[pop]) {
+                if (!s.empty() && popV[pop] == s.top()) {
+                    s.pop();
+                    ++pop;
+                } else {
+                    s.push(pushV[push]);
+                    ++push;
+                }
+            } else if (push < pushV.size() && pushV[push] == popV[pop]) {
+                ++push;
+                ++pop;
+            } else {
+                if (!s.empty() && s.top() == popV[pop]) {
+                    s.pop();
+                    ++pop;
+                }  else {
+                    return false;
+                }
+            }    
+        }
+        return s.empty();
+    }
+};
