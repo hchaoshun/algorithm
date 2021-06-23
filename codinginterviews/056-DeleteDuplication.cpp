7b2475e357fad2b7c1bda0e3bf22e3a6ea226518
hchaoshun <hcsxiaohan@gmail.com>
2021-06-23 21:16:31 +0800

    056

--- codinginterviews/056-DeleteDuplication.cpp
+++ codinginterviews/056-DeleteDuplication.cpp
@@ -29,3 +29,34 @@ public:
         return dummy.next;
     }
 };
+
+//用flag表示是否需要删除
+class Solution {
+public:
+    ListNode* deleteDuplication(ListNode* pHead) {
+        ListNode dummy = ListNode(-1);
+        dummy.next = pHead;
+        ListNode* p1 = &dummy;
+        ListNode* p2 = pHead;
+        bool flag = false;
+        while (p2 != nullptr) {
+            if (p2->next != nullptr && p2->next->val != p2->val) {
+                if (flag == true) {
+                    p1->next = p2->next;
+                    flag = false;
+                } else {
+                    p1 = p2;
+                }
+
+            } else if (p2->next != nullptr && p2->next->val == p2->val) {
+               flag = true;
+            }
+            p2 = p2->next;
+        }
+        //可能会出现最后一次没有删除
+        if (flag == true) {
+            p1->next = p2;
+        }
+        return dummy.next;
+    }
+};
