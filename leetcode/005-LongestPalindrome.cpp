fb15f4b605657da3832c2e23f1fef66c0cb41668
hchaoshun <hcsxiaohan@gmail.com>
2021-07-12 21:50:05 +0800

    dp

--- leetcode/005-LongestPalindrome.cpp
+++ leetcode/005-LongestPalindrome.cpp
@@ -1,3 +1,7 @@
+//像滑动窗口一样，依次找窗口为2,3...的字串是否是回文，这样遍历到最后的最大窗口就是最终结果
+//bool dp[i][j] 记录i到j的字串是否是回文,
+//dp[i][j] = (s[i] == s[j] && (dp[i+1][j-1] || j - 1 == 1))
+
 class Solution {
 public:
     string longestPalindrome(string s) {
