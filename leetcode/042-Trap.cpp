2591805954febcf2da214c51d456e232df73a612
hchaoshun <hcsxiaohan@gmail.com>
2021-07-14 21:44:37 +0800

    greedy

--- leetcode/042-Trap.cpp
+++ leetcode/042-Trap.cpp
@@ -20,3 +20,27 @@ public:
         return result;
     }
 };
+
+//先分别存储左右两边的最大高度，
+//每个元素的结果为min(left[i-1], right[i+1]) - height[i]
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        const int n = height.size();
+        if (n == 0) return 0;
+        vector<int> left(n, 0), right(n, 0);
+        left[0] = height[0], right[n-1] = height[n-1];
+        for (int i = 1; i < n; ++i) {
+            left[i] = max(height[i], left[i-1]);
+            right[n-i-1] = max(height[n-i-1], right[n-i]);
+        }
+        int result = 0;
+        for (int i = 1; i < n - 1; ++i) {
+            int min_h = min(left[i-1], right[i+1]);
+            if (height[i] < min_h) {
+                result += min_h - height[i];
+            }
+        }
+        return result;
+    }
+};
